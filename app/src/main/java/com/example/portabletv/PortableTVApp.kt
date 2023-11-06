5c484a501ba5de44821557a3d8d71fa55358ab21
Pankaj Singh <pankajsingh13042000@gmail.com>
2023-11-06 19:40:12 +0530

    Implemented coil cache for posters images

--- app/src/main/java/com/example/portabletv/PortableTVApp.kt
+++ app/src/main/java/com/example/portabletv/PortableTVApp.kt
@@ -1,7 +1,34 @@
 package com.example.portabletv
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.request.CachePolicy
+import coil.util.DebugLogger
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class PortableTVApp: Application()
+class PortableTVApp: Application(), ImageLoaderFactory {
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader (this)
+            .newBuilder()
+            .memoryCachePolicy(CachePolicy.ENABLED)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.1)
+                    .strongReferencesEnabled(true)
+                    .build()
+            }
+            .diskCachePolicy(CachePolicy.ENABLED)
+            .diskCache {
+                DiskCache.Builder()
+                    .maxSizePercent(0.03)
+                    .directory(cacheDir)
+                    .build()
+            }
+            .logger(DebugLogger())
+            .build()
+    }
+}
