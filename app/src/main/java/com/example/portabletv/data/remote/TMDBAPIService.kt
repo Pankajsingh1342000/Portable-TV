ec3e5f53ae6a1a17838d4d59d682048efc1ea0d2
Pankaj Singh <pankajsingh13042000@gmail.com>
2023-11-10 15:55:48 +0530

    solve the issue of API KEY

--- app/src/main/java/com/example/portabletv/data/remote/TMDBAPIService.kt
+++ app/src/main/java/com/example/portabletv/data/remote/TMDBAPIService.kt
@@ -3,15 +3,21 @@ package com.example.portabletv.data.remote
 import com.example.portabletv.data.remote.models.trending_movies_model.TrendingMoviesResponse
 import com.example.portabletv.utils.GET_TENDING_TV_SHOWS
 import com.example.portabletv.data.remote.models.trending_tv_show_model.TrendingTVShowsResponse
+import com.example.portabletv.utils.API_KEY
 import com.example.portabletv.utils.GET_TENDING_MOVIES
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface TMDBAPIService {
 
     @GET(GET_TENDING_TV_SHOWS)
-    suspend fun getTrendingTVShows(): TrendingTVShowsResponse
+    suspend fun getTrendingTVShows(
+        @Query("api_key") api_key: String = API_KEY
+    ): TrendingTVShowsResponse
 
     @GET(GET_TENDING_MOVIES)
-    suspend fun getTrendingMovies(): TrendingMoviesResponse
+    suspend fun getTrendingMovies(
+        @Query("api_key") api_key: String = API_KEY
+    ): TrendingMoviesResponse
 
 }
--- app/src/main/java/com/example/portabletv/utils/constants.kt
+++ app/src/main/java/com/example/portabletv/utils/constants.kt
@@ -1,6 +1,12 @@
 package com.example.portabletv.utils
 
 const val BASE_URL = "https://api.themoviedb.org/3/"
-const val GET_TENDING_TV_SHOWS = "trending/tv/week?api_key=39609b669e09dfe6839f8c0c071f0906"
-const val GET_TENDING_MOVIES = "trending/movie/day?api_key=39609b669e09dfe6839f8c0c071f0906"
-//const val API_KEY = ""
+
+const val GET_TENDING_TV_SHOWS = "trending/tv/week"
+const val GET_TENDING_MOVIES = "trending/movie/day"
+
+const val API_KEY = "39609b669e09dfe6839f8c0c071f0906"
+
+
+//const val GET_TENDING_TV_SHOWS = "trending/tv/week?api_key=39609b669e09dfe6839f8c0c071f0906"
+//const val GET_TENDING_MOVIES = "trending/movie/day?api_key=39609b669e09dfe6839f8c0c071f0906"
